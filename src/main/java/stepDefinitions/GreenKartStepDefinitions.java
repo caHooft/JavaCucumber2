53054fd7e2e03b7808a7ae6c9b8ea79ef0c92b8c
Casper Hooft <casper.hooft@ordina.nl>
2024-06-07 13:25:34 +0200

    Section 8 last video. Still some incompatibility problems
    Signed-off-by: Casper Hooft <casper.hooft@ordina.nl>

--- src/main/java/stepDefinitions/GreenKartStepDefinitions.java
+++ src/main/java/stepDefinitions/GreenKartStepDefinitions.java
@@ -1,23 +1,30 @@
 package stepDefinitions;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class GreenKartStepDefinitions {
+    public WebDriver driver;
+
+
     @Given("user is on greencart landing page")
     public void user_is_on_greencart_landing_page() {
         System.setProperty("webdriver.chrome.driver", "C:/bin/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
     }
 
     @When("user searched with shortname {string} and finds actual name of the product")
-    public void user_searched_with_shortname_and_finds_actual_name_of_the_product(String string) {
+    public void user_searched_with_shortname_and_finds_actual_name_of_the_product(String shortname) {
 
-
+        driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortname);
+        String productName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
+        System.out.println(productName + "is extracted from home page");
     }
 
     @Then("user searches for the same product with the same shortname in office space")
